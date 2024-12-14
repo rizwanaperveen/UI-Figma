458dfbbd36a7f13a55aa3b5af0fa28a7989a4e8e
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-12-15 01:07:13 +0500

    Update Sofa.tsx
    add link on button

--- src/components/Sofa.tsx
+++ src/components/Sofa.tsx
@@ -1,11 +1,23 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Sofa = () => {
+  const [showItem, setShowItem] = useState(false);
+  const handleOrder = () => {
+    setShowItem(showItem === true ? false : true);
+  }
   return (
     <div>
-     <div className='w-[1476px] h-[799px] flex justify-center items-center ml-20'>
-        <Image src="/Group23.png" alt="" width={1476} height={799} />
+     <div className='w-[1476px] h-[799px] sm:mb-5 flex justify-center items-center bg-[#FFF9E5]'>
+        <Image src="/Group23a.png" alt="image" width={983} height={799} />
+        <div className='w-[331px] h-[205px] text-center space-y-6'>
+          <h2 className='w-[149px] h-[36px] text-2xl ml-20'>New Arrivals</h2>
+          <h1 className='w-[331px] h-[72px] text-5xl font-bold'>Asgaard sofa</h1>
+          <Link href="../singleItem"><button className='w-[255px] h-[64px] text-xl border-black border-2' onClick={handleOrder}>Order Now</button></Link>
+        </div>
      </div>
 
     </div>
